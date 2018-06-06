dd13ce9b3e99ddc5b69b7d7db890696ef17b62cf
Pawel Orlikoski <orliktcz@gmail.com>
2018-06-06 15:41:07 +0200

    Zadanie 1

---
+++ src/com/infoshareacademy/Main.java
@@ -0,0 +1,13 @@
+package com.infoshareacademy;
+
+
+public class Main {
+
+    public static int iloczyn(int a,int b){
+        return a*b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(iloczyn(3,4));
+    }
+}
