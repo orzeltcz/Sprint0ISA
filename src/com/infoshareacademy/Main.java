3e2f1be79effaed360a541b2de7d60ef16152745
Pawel Orlikoski <orliktcz@gmail.com>
2018-06-06 16:08:06 +0200

    Zadania od 1 do 4

--- src/com/infoshareacademy/Main.java
+++ src/com/infoshareacademy/Main.java
@@ -2,12 +2,27 @@ package com.infoshareacademy;
 
 
 public class Main {
-
+    //Funkcja mnożenia z operatorem *
     public static int iloczyn(int a,int b){
         return a*b;
     }
 
+    //funkcja mnożenia bez operatora *
+    public static int iloczynSuma(int a,int b) {
+        int suma=0;
+        int h = b;
+        if (h < 0) h = -h;
+
+        for (int i = 0; i < h; i++) {
+            suma = suma + a;
+        }
+        if (b < 0) return -suma;
+        else return suma;
+    }
+
+
     public static void main(String[] args) {
-        System.out.println(iloczyn(3,4));
+        System.out.println(iloczyn(3,-4));
+        System.out.println(iloczynSuma(-2,-3));
     }
 }
