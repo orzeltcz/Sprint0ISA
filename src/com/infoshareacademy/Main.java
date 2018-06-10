0a4cab60e2be9340e166596735cf5f66b05b6ea1
orzeltcz <orliktcz@gmail.com>
2018-06-10 17:26:38 +0200

    zadanie 3 poprawione bez tablicy przedzialow

--- src/com/infoshareacademy/Main.java
+++ src/com/infoshareacademy/Main.java
@@ -8,15 +8,14 @@ import com.infoshareacademy.task4.Task4;
 import com.infoshareacademy.task5.Task5;
 import com.infoshareacademy.task6.Task6;
 
-import java.util.*;
+
 
 public class Main {
 
 
     public static void main(String[] args) {
-        int a=Task6.fibonnaci(19);
-        System.out.println(a);
-
+Task3 task3 = new Task3();
+task3.przedzialy();
         }
 
     }
